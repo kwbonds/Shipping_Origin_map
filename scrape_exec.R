d87575d38d32f7ebe6a467cac0fd703cf57a0077
kwbonds <kevin.w.bonds@gmail.com>
2016-12-01 12:07:57 -0800

    Clean and split text string for lon an lat in vessel_scrape.R

--- scrape_exec.R
+++ scrape_exec.R
@@ -1,18 +1,23 @@
 library(plyr)
 library(dplyr)
+library(tidyr)
 require(RSelenium)
-pJS <- phantom(pjs_cmd = "C:\\Users\\Ke2l8b1\\Documents\\Shipping_origin_map\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe")
+# pJS <- phantom(pjs_cmd = "C:\\Users\\Ke2l8b1\\Documents\\Shipping_origin_map\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe")
 Sys.sleep(5)
 # eCap <- list(phantomjs.page.settings.loadImages = FALSE)
 # remDr <- remoteDriver(browserName = "phantomjs", extraCapabilities = eCap)
 # remDr$open()
-vessels <- c("CONCORDIA", "HYUNDAI PREMIUM", "MALLECO")
+vessels <- c("CONCORDIA", "HYUNDAI PREMIUM", "MALLECO", "Not a ship")
 source("vessel_scrape.R")
 vessel_table <- ldply(vessels, function(x) scrape(x), .progress = "text", .inform = TRUE)
 
-vessel_table <- ldply("HYUNDAI PREMIUM", function(x) scrape(x), .progress = "text")
+vessel_table2 <- ldply(vessels, function(x) scrape(x), .progress = "text", .inform = TRUE)
 
-scrape("HYUNDAI PREMIUM")
+vessel_table <- ldply("HYUNDAI PREMIUM", function(x) scrape(x), .progress = "text", .inform = TRUE)
+vessel_table2 <- ldply("CONCORDIA", function(x) scrape(x), .progress = "text", .inform = TRUE)
+vessel_table3 <- ldply("MALLECO", function(x) scrape(x), .progress = "text", .inform = TRUE)
+
+scrape("Zim Rotterdam")
 remDr$getCurrentURL()
 scrape("CONCORDIA")
 
--- vessel_scrape.R
+++ vessel_scrape.R
@@ -29,16 +29,26 @@ scrape <- function(boat){
   # my_source <- remDr$getPageSource()
   
   # f_lat <- substr(my_source, as.numeric(gregexpr(pattern ='latitude...',my_source))+ 10, stop = as.numeric(gregexpr(pattern ='latitude...',my_source)) +10 + 10)
-  lat <- substr(my_source, as.numeric(gregexpr(pattern ='latitude',my_source)) + 10, stop = as.numeric(gregexpr(pattern ='latitude',my_source)) + 20)
+  a_lat <- substr(my_source, as.numeric(gregexpr(pattern ='latitude">',my_source))+10, stop = as.numeric(gregexpr(pattern ='latitude">',my_source)) + 20)
+  if(grepl('<',a_lat)) {a_lat <- substr(a_lat, 1, regexpr('<', a_lat)-1)}
+  a_lat <- unlist(strsplit(a_lat, " "))
+  lat <- a_lat[1]
+  lat_hemi <-  a_lat[2]
   
-  lon <- substr(my_source, as.numeric(gregexpr(pattern ='longitude',my_source)) + 11, stop = as.numeric(gregexpr(pattern ='longitude',my_source)) + 21)
-
+  a_lon <- substr(my_source, as.numeric(gregexpr(pattern ='longitude">',my_source)) + 11, stop = as.numeric(gregexpr(pattern ='longitude">',my_source)) + 21)
+  if(grepl('<', a_lon)) { a_lon <- substr(a_lon, 1, regexpr('<', a_lon)-1) }
+  a_lon <- unlist(strsplit(a_lon, " "))
+  lon <- a_lon[1]
+  lon_hemi <-  a_lon[2]
+  
   course <- substr(my_source, as.numeric(gregexpr(pattern ='Course/Speed',my_source)) + 95, stop = as.numeric(gregexpr(pattern ='Course/Speed',my_source)) + 102)
-  
+  # course <- substr(my_source, as.numeric(gregexpr(pattern =' °&nbsp',my_source)) -5, stop = as.numeric(gregexpr(pattern =' °&nbsp',my_source))-1)
+  course <- trimws(course)
+  vessel <- substr(my_source, as.numeric(gregexpr(pattern ='og:title',my_source)) + 19, stop = as.numeric(gregexpr(pattern =' - see',my_source))-13)
   
   remDr$close()
   pJS$stop()
   Sys.sleep(5)
-  return(c(boat, lat, lon, course))
+  return(c(boat, vessel, lon, lon_hemi, lat, lat_hemi, course))
 }
 
